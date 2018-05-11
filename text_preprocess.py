4152b1fd7b55fccdd577b56190ba668667f2bba9
rohan8594 <rohan.8594@gmail.com>
2018-05-10 22:14:18 -0700

    added new column to processed_msgs dataframe

--- text_preprocess.py
+++ text_preprocess.py
@@ -5,15 +5,16 @@
 # Date:        05/09/2018
 #
 # Description: This script loads the sms spam data, organizes the data into a pandas dataframe, adds a new 
-#              feature (length of message) to the data, and applies some basic text pre-processing techniques 
-#              like stopword removal and punctuation removal. Finally, the processed dataframe is copied into 
-#              a new csv file processed_msgs.csv
+#              feature (length of message) to the data, applies some basic text pre-processing techniques 
+#              like stopword removal and punctuation removal, and converts the messages into a list of processed 
+#              tokens. Finally, the processed dataframe is copied into a new csv file processed_msgs.csv
 
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
+nltk.download('stopwords')
 
 def loadData():
     
@@ -32,7 +33,6 @@ def text_process(data):
     nopunc = [c for c in data if c not in string.punctuation] #remove punctuations
     nopunc = ''.join(nopunc)
     
-    #nltk.download('stopwords')
     clean_msgs = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')] # remove stopwords
     
     return clean_msgs
@@ -41,10 +41,13 @@ def main():
     
     messages = loadData()
     #print(messages)
-    messages['message'] = messages['message'].apply(text_process)
+    messages['processed_msg'] = messages['message'].apply(text_process)
     
-    print('\n######################## Processed Messages #########################\n')
-    print(messages)
+    print('\n################################################## Processed Messages ##################################################\n')
+    with pd.option_context('expand_frame_repr', False):
+    	print (messages)
+    #print(messages)
+
     messages.to_csv('output/processed_msgs.csv', encoding='utf-8', index=False) #copy processed messages dataframe to a new csv file
 
 if __name__ == "__main__":
