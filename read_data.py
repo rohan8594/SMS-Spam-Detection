06b48d3f54eb648e7905574cf602ac3f65488215
rohan8594 <rohan.8594@gmail.com>
2018-05-10 18:32:12 -0700

    added new files

---
+++ read_data.py
@@ -0,0 +1,19 @@
+# File:        read_data.py
+#
+# Author:      Rohan Patel
+#
+# Date:        05/09/2018
+#
+# Description: This script simply reads the sms data from the SMSSpamCollection file, prints the total number of
+#			   sms messages in the dataset, and then individually prints the first 100 lines from the SMSSpamCollection
+#			   file. The purpose of this script is to simply give an initial idea of how the sms data is organized in 
+#			   then dataset.
+
+messages = [line.rstrip() for line in open('smsspamcollection/SMSSpamCollection')]
+
+print('\nTotal number of messages:' + str(len(messages)))
+print('\n')
+
+for messno, msg in enumerate(messages[:100]):
+    print(messno, msg)
+    print('\n')
