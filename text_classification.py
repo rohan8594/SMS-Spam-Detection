4c21566fd8e75f65ed974401ee4469a2726cd125
rohan8594 <rohan.8594@gmail.com>
2018-05-12 01:16:35 -0700

    added train_classifier and predict functions, and trained various classifiers

--- text_classification.py
+++ text_classification.py
@@ -12,7 +12,13 @@ import text_preprocessing
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
@@ -40,6 +46,12 @@ def TrainTestSplit(tfidf_vect, messages):
     
     return X_train, X_test, y_train, y_test
 
+def train_classifier(classifier, X_train, y_train):
+    classifier.fit(X_train, y_train)
+    
+def predict_labels(classifier, X_test):
+    return (classifier.predict(X_test))
+
 def main():
     
     messages = pd.read_csv('output/processed_msgs.csv')
@@ -47,14 +59,39 @@ def main():
     tfidf_vect = Tfidf_Vectorization(messages)
     X_train, X_test, y_train, y_test = TrainTestSplit(tfidf_vect, messages)
     
-    mnb = MultinomialNB()
-    mnb.fit(X_train, y_train)
-    pred = mnb.predict(X_test)
+    svm = SVC(kernel = 'sigmoid', gamma = 1.0)
+    dtree = DecisionTreeClassifier()
+    mnb = MultinomialNB(alpha = 0.2)
+    knn = KNeighborsClassifier()
+    rfc = RandomForestClassifier()
+    ada_boost = AdaBoostClassifier()
+    bagging_clf = BaggingClassifier()
+    
+    classifiers = {'SVM': svm, 'Decision Tree': dtree, 'Multinomial NB': mnb, 'KNN': knn, 'Random Forest': rfc, 
+                   'AdaBoost': ada_boost, 'Bagging Classifier': bagging_clf}
+    
+    pred_scores = []
+    for k, v in classifiers.items():
+        train_classifier(v, X_train, y_train)
+        pred = predict_labels(v, X_test)
+        pred_scores.append((k, [accuracy_score(y_test, pred)]))
+        print('\n############### ' + k + ' ###############\n')
+        print(classification_report(y_test, pred))
+        
+    accuracy = pd.DataFrame.from_items(pred_scores, orient = 'index', columns = ['Accuracy Rate'])
+    print('\n')
+    print(accuracy)
+    
+    '''
+    train_classifier(mnb, X_train, y_train)
+    pred = predict_labels(mnb, X_test)
+    
     print(pred)
     print('\n')
     print('Accuracy Score:', accuracy_score(y_test, pred))
     print('\n')
     print(classification_report(y_test, pred))
+    '''
     
 if __name__ == "__main__":
     main()
